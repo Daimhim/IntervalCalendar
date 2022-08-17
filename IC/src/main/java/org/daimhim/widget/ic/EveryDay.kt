048323f0fc102b664f443dc039519856f343a8cc
Daimhim <daimhim@163.com>
2022-08-17 16:02:45 +0800

    1. 更新数据Bean V2

--- IC/src/main/java/org/daimhim/widget/ic/EveryDay.kt
+++ IC/src/main/java/org/daimhim/widget/ic/EveryDay.kt
@@ -4,16 +4,24 @@ class EveryDay {
     /**
      * Item 样式
      */
+    @JvmField
     var viewType = 0
     /**
      * 是否可用
      */
+    @JvmField
     var enable = true
+    @JvmField
     var millis: Long = -1
     var year = 0
+    @JvmField
     var moon = 0
+    @JvmField
     var day = 0
+    @JvmField
     var hour = 0
+    @JvmField
     var minute = 0
+    @JvmField
     var second = 0
 }
